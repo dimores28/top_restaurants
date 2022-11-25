8875c5241403b58d2ce46dbe68374f5bf882ba65
Dmytro <dimores95@gmail.com>
2022-11-25 16:22:30 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <Nav />
+  <div class="container">
+    <h1>Welcome to Restaurantly</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+      crossorigin: ""
+    }
+  ]
+});
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
